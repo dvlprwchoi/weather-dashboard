531ce40aac0e82d406b2a0971d458372dff474c7
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-13 19:55:30 -0600

    Update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,18 @@
 function Footer() {
-  return <div className="Footer">This is Footer</div>;
+  return (
+    <div className="Footer">
+      Built by{' '}
+      <a
+        className="portfolio-link"
+        href="https://woosikchoi.com"
+        target="_blank"
+        rel="noopenner noreferrer"
+      >
+        WOO-SIK CHOI
+      </a>{' '}
+      in December 2021
+    </div>
+  );
 }
 
 export default Footer;
