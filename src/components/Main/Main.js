da3bf83613fb4815ea6668e68e80d1c8dc1a4a1a
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-14 10:39:15 -0600

    Add place holder in Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,14 @@
 function Main() {
-  return <div className="Main">This is Main</div>;
+  return (
+    <div className="Main">
+      <>
+        <div className="weather-icon">sunny</div>
+        <div className="temperature">73</div>
+        <div className="location-input">Austin, TX</div>
+        <div className="carousel">1234</div>
+      </>
+    </div>
+  );
 }
 
 export default Main;
