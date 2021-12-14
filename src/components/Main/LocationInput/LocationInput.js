13b0dc03a4846d4c8fd7ac9b20940a90a7c3025d
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-14 12:04:44 -0600

    Add submit button and input box

--- src/components/Main/LocationInput/LocationInput.js
+++ src/components/Main/LocationInput/LocationInput.js
@@ -1,5 +1,17 @@
 function LocationInput() {
-  return <div className="location-input">Austin, TX</div>;
+  return (
+    <div className="location-input">
+      <label className="locationInputBox" for="location">
+        <input
+          type="text"
+          placeholder="city"
+          name="location"
+          id="location"
+        ></input>
+      </label>
+      <button className="locationInputSubmitButton">Submit</button>
+    </div>
+  );
 }
 
 export default LocationInput;
