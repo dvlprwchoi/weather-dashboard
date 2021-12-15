2c23dd542c6e281abd5f27abd922a7024878a575
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-15 15:26:40 -0600

    Add submit function and prop down API to location input.js

--- src/components/Main/LocationInput/LocationInput.js
+++ src/components/Main/LocationInput/LocationInput.js
@@ -1,7 +1,28 @@
-function LocationInput() {
+function LocationInput({ API_URL }) {
+  // fetch(API_URL)
+  //   .then((res) => res.json())
+  //   .then((data) => console.log(data));
+
+  // const locationInput = document.querySelector('#location');
+
+  const _submit = async (e) => {
+    e.preventDefault();
+    console.log('from the event');
+    const zipCode = this.state.value;
+    console.log(zipCode);
+
+    try {
+      const response = await fetch({ API_URL });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="location-input">
-      <form className="locationInputBoxAndSubmitButton">
+      <form className="locationInputBoxAndSubmitButton" onSubmit={_submit}>
         <label className="locationInputLabel" htmlFor="location">
           <input
             type="text"
