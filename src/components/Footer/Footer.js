efe3bbf4ee46ffbf4967e4f8112922b569f1ce55
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-15 14:09:36 -0600

    Add weather api credit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,16 +2,28 @@ function Footer() {
   return (
     <div className="Footer">
       <div className="footer-content">
-        Built by{' '}
-        <a
-          className="portfolio-link"
-          href="https://woosikchoi.com"
-          target="_blank"
-          rel="noopenner noreferrer"
-        >
-          WOO-SIK CHOI
-        </a>{' '}
-        in December 2021
+        <p className="main-footer">
+          Built by{' '}
+          <a
+            className="portfolio-link"
+            href="https://woosikchoi.com"
+            target="_blank"
+            rel="noopenner noreferrer"
+          >
+            WOO-SIK CHOI
+          </a>{' '}
+          in December 2021
+        </p>
+        <p className="credit">
+          Powered by
+          <a
+            className="credit-link"
+            href="https://www.weatherapi.com/"
+            title="Weather API"
+          >
+            WeatherAPI.com
+          </a>
+        </p>
       </div>
     </div>
   );
