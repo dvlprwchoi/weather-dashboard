cb59ddfec66f8f2f229a1b11b298d10f53626f8d
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-16 16:17:12 -0600

    Disable weather output classname

--- src/components/Main/WeatherOutput/WeatherOutput.js
+++ src/components/Main/WeatherOutput/WeatherOutput.js
@@ -2,37 +2,37 @@ function WeatherOutput({ weather }) {
   if (weather.length === 0) {
     return (
       <>
-        <div className="weather-output">
-          <div className="weather-icon">
-            <img
-              className="weather-icon-img"
-              src="./android-chrome-192x192.png"
-              alt="weather-icon-img"
-            />
-          </div>
-          <div className="temperature">
-            <div className="temperature-digit">???</div>
-          </div>
+        {/* <div className="weather-output"> */}
+        <div className="weather-icon">
+          <img
+            className="weather-icon-img"
+            src="./android-chrome-192x192.png"
+            alt="weather-icon-img"
+          />
         </div>
+        <div className="temperature">
+          <div className="temperature-digit">???</div>
+        </div>
+        {/* </div> */}
       </>
     );
   } else {
     return (
       <>
-        <div className="weather-output">
-          <div className="weather-icon">
-            <img
-              className="weather-icon-img"
-              src={weather.current.condition.icon}
-              alt="weather-icon-img"
-            />
-          </div>
-          <div className="temperature">
-            <div className="temperature-digit">
-              {weather.current.temp_f + ' F'}
-            </div>
+        {/* <div className="weather-output"> */}
+        <div className="weather-icon">
+          <img
+            className="weather-icon-img"
+            src={weather.current.condition.icon}
+            alt="weather-icon-img"
+          />
+        </div>
+        <div className="temperature">
+          <div className="temperature-digit">
+            {weather.current.temp_f + ' F'}
           </div>
         </div>
+        {/* </div> */}
       </>
     );
   }
