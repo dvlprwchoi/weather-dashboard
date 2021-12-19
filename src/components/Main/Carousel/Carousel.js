e5846c207248895a6aad9fafea1f99f9ccd3b9da
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-19 08:35:33 -0600

    Add carousel functionality

--- src/components/Main/Carousel/Carousel.js
+++ src/components/Main/Carousel/Carousel.js
@@ -5,8 +5,66 @@ function Carousel({ weather, isCelcious }) {
   return (
     <div className="carousel">
       <div className="carousel-background">
-        <WeatherDetailCard weather={weather} isCelcious={isCelcious} />
-        <Forecast weather={weather} isCelcious={isCelcious} />
+        <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
+          <div className="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#myCarousel"
+              data-bs-slide-to="0"
+              className="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#myCarousel"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            ></button>
+            {/* <button
+              type="button"
+              data-bs-target="#myCarousel"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button> */}
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              {/* <div className="container"> */}
+              <WeatherDetailCard weather={weather} isCelcious={isCelcious} />
+              {/* </div> */}
+            </div>
+            <div className="carousel-item">
+              {/* <div className="container"> */}
+              <Forecast weather={weather} isCelcious={isCelcious} />
+              {/* </div> */}
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#myCarousel"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#myCarousel"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
     </div>
   );
